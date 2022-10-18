3b33f99d2057341afd8aab2dfc44f5af2c600097
xyy277 <aj907507646@qq.com>
2022-10-18 11:44:51 +0800

    fix: model json transform

--- global/model.go
+++ global/model.go
@@ -8,7 +8,7 @@ import (
 )
 
 type GS_BASE_MODEL_ID_UINT struct {
-	ID            uint           `gorm:"primarykey" json:"id,string" form:"id"` // 主键ID
+	ID            uint           `gorm:"primarykey" json:"id,string" form:"id"` // 主键ID, uint json 需要指定转为string
 	CreatorId     string         `json:"creator_id"  gorm:"comment:创建人id"`
 	Creator       string         `json:"creator"  gorm:"comment:创建人"`
 	LastUpdaterId string         `json:"last_update_id"  gorm:"comment:更新人id"`
@@ -20,7 +20,7 @@ type GS_BASE_MODEL_ID_UINT struct {
 }
 
 type GS_BASE_MODEL_ID_STRING struct {
-	ID            string         `gorm:"primarykey" json:"id,string" form:"id"` // 主键ID
+	ID            string         `gorm:"primarykey" json:"id" form:"id"` // 主键ID
 	CreatorId     string         `json:"creator_id"  gorm:"comment:创建人id"`
 	Creator       string         `json:"creator"  gorm:"comment:创建人"`
 	LastUpdaterId string         `json:"last_update_id"  gorm:"comment:更新人id"`
@@ -32,7 +32,7 @@ type GS_BASE_MODEL_ID_STRING struct {
 }
 
 type GS_BASE_MODEL_ID_NO_PRIMARY struct {
-	Id            string         `json:"id,string" form:"id"` // ID
+	Id            string         `json:"id" form:"id"` // ID
 	CreatorId     string         `json:"creator_id"  gorm:"comment:创建人id"`
 	Creator       string         `json:"creator"  gorm:"comment:创建人"`
 	LastUpdaterId string         `json:"last_update_id"  gorm:"comment:更新人id"`
@@ -58,7 +58,7 @@ type GS_BASE_MODEL_ID_NONE struct {
 type GS_BASE_USER struct {
 	// 适用于一些数据库自增性主键，数据库存储一般为数值，某些数据库可能不支持，比如cockroachdb并不会自增，这中ID在Mysql数据库中有更好的应用
 	// ID string `gorm:"primarykey" json:"id,string" form:"id"` // 主键ID
-	ID uuid.UUID `gorm:"primarykey" json:"id,string" form:"id"` // 主键ID
+	ID uuid.UUID `gorm:"primarykey" json:"id" form:"id"` // 主键ID
 	// UUID 通用的标准用户id，数据库存储为字符串在某些方面会更通用更好用
 	// UUID uuid.UUID `json:"uuid" gorm:"comment:用户UUID"`
 	// 用户名
